73aedc42d766a64af4d5e69c289a380dd423f8ed
zaqueu.cavalcante <zaqueu.cavalcante@exato.digital>
2025-12-24 15:33:43 -0300

    Add bits-ui lib

--- App/src/routes/+page.svelte
+++ App/src/routes/+page.svelte
@@ -1,1 +1,24 @@
+<script lang="ts">
+  import { Accordion } from "bits-ui";
+</script>
+
 <h1>Welcome to ESTUD!</h1>
+
+<Accordion.Root type="single">
+  <Accordion.Item value="item-1">
+    <Accordion.Header>
+      <Accordion.Trigger>Students</Accordion.Trigger>
+    </Accordion.Header>
+    <Accordion.Content
+      >This is the collapsible content for this section.</Accordion.Content
+    >
+  </Accordion.Item>
+  <Accordion.Item value="item-2">
+    <Accordion.Header>
+      <Accordion.Trigger>Teachers</Accordion.Trigger>
+    </Accordion.Header>
+    <Accordion.Content
+      >This is the collapsible content for this section.</Accordion.Content
+    >
+  </Accordion.Item>
+</Accordion.Root>
