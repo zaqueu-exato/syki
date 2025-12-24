d05632d2b78eb4e14e265a330dd1074be3bd0825
zaqueu.cavalcante <zaqueu.cavalcante@exato.digital>
2025-12-24 15:14:10 -0300

    Save

--- App/src/routes/+layout.svelte
+++ App/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
@@ -7,5 +8,37 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/blog">blog</a>
+	<a href="/message">message</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>---</h1>
+
+<p>
+	URL = {page.url}
+</p>
+<p>
+	PARAMS = {JSON.stringify(page.params)}
+</p>
+<p>
+	ROUTE = {JSON.stringify(page.route)}
+</p>
+<p>
+	STATE = {JSON.stringify(page.state)}
+</p>
+<p>
+	DATA = {JSON.stringify(page.data)}
+</p>
+<p>
+	STATUS = {JSON.stringify(page.status)}
+</p>
+<p>
+	ERROR = {JSON.stringify(page.error)}
+</p>
+
 
 {@render children()}
